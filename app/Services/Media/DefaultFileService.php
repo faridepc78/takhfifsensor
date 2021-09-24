b1871bb4079fdc9b905c2d21eacb042d1a7008bb
faridepc78 <faridnewepc78@gmail.com>
2021-09-24 03:58:47 +0330

    commit-21

--- app/Services/Media/DefaultFileService.php
+++ app/Services/Media/DefaultFileService.php
@@ -13,9 +13,9 @@ abstract class DefaultFileService
         foreach ($media->files as $file) {
             $key = 'uploads';
             if (empty($media->private_folder)) {
-                unlink($key . '\\' . $file);
+                unlink($key . '/' . $file);
             } else {
-                unlink($key . '\\' . $file);
+                unlink($key . '/' . $file);
                 $full_path = $key . '/' . $media->public_folder . '/' . $media->private_folder;
                 if (is_dir_empty($full_path)) {
                     rmdir($full_path);
