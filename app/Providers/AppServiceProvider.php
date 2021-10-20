2a5648d0a1a75ee1cf29a490b96a70121f106f0d
faridepc78 <faridnewepc78@gmail.com>
2021-10-20 14:27:59 +0330

    commit-41

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
+use URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +26,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (App::environment(['staging', 'production'])) {
+            URL::forceScheme('https');
+        }
         Paginator::useBootstrap();
     }
 }
